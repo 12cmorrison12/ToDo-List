d6c042369c5ee54cdc51485516f0578c7340cdb9
unknown <12cmorrison12@gmail.com>
2022-09-23 17:11:19 -0600

    made style sheet and function for toggling class

---
+++ app.js
@@ -0,0 +1,15 @@
+/* As a user, you should be able to:
+
+Add a new todo (by submitting a form)
+Mark a todo as completed (cross out the text of the todo)
+Remove a todo */
+
+function toggleToDos() {
+    const toDo = document.querySelectorAll('li');
+
+    for (let li of toDo) {
+        if (li.classList === '') {
+            li.classList.toggle('finished');
+        }
+    }
+}
