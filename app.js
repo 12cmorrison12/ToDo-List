423b1a0475ac022494fdc1a111f56f9be706a41f
unknown <12cmorrison12@gmail.com>
2022-09-27 12:54:03 -0600

    finished remove and add buttons

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ Add a new todo (by submitting a form)
 Mark a todo as completed (cross out the text of the todo)
 Remove a todo */
 
-function toggleToDos() {
-    const toDo = document.querySelectorAll('li');
+const toDoList = document.querySelector('ul');
+const submitList = document.getElementById('listform');
 
-    for (let li of toDo) {
-        li.classList.toggle('finished');
+//to add an item via the submit button form
+submitList.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const newToDo = document.createElement('li');
+    toDoList.appendChild(newToDo);
+
+    let deleteButton = document.createElement('button');
+    deleteButton.className = "delete";
+
+    newToDo.appendChild(deleteButton);
+})
+
+// to toggle strike through
+toDoList.addEventListener('click', function(e) {
+    if(e.target.className === 'li') {
+        e.target.className.toggle('finished');
+    } else if (e.target.className === 'delete') {
+        e.target.className.remove();
     }
-}
+});
